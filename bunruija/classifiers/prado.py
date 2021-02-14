8de3e07b198dbb0f25809977c58aa2fd5625dad2
Takuya Makino <takuyamakino15@gmail.com>
2021-02-14 15:36:10 +0900

    Refine the architecture of PRADO (#5)

--- bunruija/classifiers/prado.py
+++ bunruija/classifiers/prado.py
@@ -13,16 +13,11 @@ class Hasher:
 
     def get_hash_codes(self, word):
         hash_ = mmh3.hash128(word)
-
-#         for i in range(0, self.n_features, 64):
-#             hash_ = mmh3.hash128(word)
-#             print(word, hash_)
-
         return hash_
 
 
 class ProjectedAttention(torch.nn.Module):
-    def __init__(self, kernel_sizes, dim_hid):
+    def __init__(self, kernel_sizes, dim_hid, skip_bigrams=None):
         super().__init__()
         self.dim_hid = dim_hid
 
@@ -32,9 +27,18 @@ class ProjectedAttention(torch.nn.Module):
                 self.dim_hid,
                 kernel_size=(kernel_size, self.dim_hid),
                 stride=1,
-                padding=(kernel_size // 2, 0),
             ) for kernel_size in kernel_sizes
         ])
+
+        if isinstance(skip_bigrams, list):
+            for c, skip_gram in zip(self.convs_value, skip_bigrams):
+                if isinstance(skip_gram, list):
+                    for i in skip_gram:
+                        torch.nn.init.zeros_(c.weight[:, :, i])
+                elif skip_gram is None:
+                    continue
+                else:
+                    raise ValueError(skip_gram)
 
         self.convs_attn = torch.nn.ModuleList([
             torch.nn.Conv2d(
@@ -42,21 +46,43 @@ class ProjectedAttention(torch.nn.Module):
                 self.dim_hid,
                 kernel_size=(kernel_size, self.dim_hid),
                 stride=1,
-                padding=(kernel_size // 2, 0),
             ) for kernel_size in kernel_sizes
+        ])
+
+        self.zero_pads = torch.nn.ModuleList([
+            torch.nn.ZeroPad2d((0, 0, kernel_size - 1, 0))
+            for kernel_size in kernel_sizes
+        ])
+
+        self.batch_norms_value = torch.nn.ModuleList([
+            torch.nn.BatchNorm1d(self.dim_hid)
+            for _ in kernel_sizes
+        ])
+
+        self.batch_norms_attn = torch.nn.ModuleList([
+            torch.nn.BatchNorm1d(self.dim_hid)
+            for _ in kernel_sizes
         ])
 
     def __call__(self, x_value_in, x_attn_in):
         x_list = []
 
-        for conv_value, conv_attn in zip(self.convs_value, self.convs_attn):
+        for conv_value, conv_attn, zero_pad, batch_norm_value, batch_norm_attn in zip(
+                self.convs_value,
+                self.convs_attn,
+                self.zero_pads,
+                self.batch_norms_value,
+                self.batch_norms_attn
+        ):
             # (bsz, output_channel, seq_len)
+            x_value_in = zero_pad(x_value_in)
             x_value = conv_value(x_value_in).squeeze(3)
-            x_value = torch.relu(x_value)
+            x_value = batch_norm_value(x_value)
 
             # (bsz, output_channel, seq_len)
+            x_attn_in = zero_pad(x_attn_in)
             x_attn = conv_attn(x_attn_in).squeeze(3)
-            x_attn = torch.relu(x_attn)
+            x_attn = batch_norm_attn(x_attn)
             x_attn = torch.softmax(x_attn, dim=2)
 
             x = x_attn * x_value
@@ -88,8 +114,15 @@ class PRADO(NeuralBaseClassifier):
         self.dropout = torch.nn.Dropout(p=dropout)
         self.distort = kwargs.get('distortion_probability', 0.25)
 
-        self.kernel_sizes = [3, 5]
-        self.attention = ProjectedAttention(self.kernel_sizes, self.dim_hid)
+        self.kernel_sizes = kwargs.get('kernel_sizes', [2, 3, 3, 4])
+        self.skip_bigrams = kwargs.get('skip_bigrams', [None, None, [1], [1, 2]]
+        self.attention = ProjectedAttention(
+            self.kernel_sizes,
+            self.dim_hid,
+            skip_bigrams=self.skip_bigrams)
+
+        self.batch_norm_value = torch.nn.BatchNorm1d(self.dim_hid)
+        self.batch_norm_attn = torch.nn.BatchNorm1d(self.dim_hid)
 
     def init_layer(self, data):
         y = []
@@ -157,16 +190,19 @@ class PRADO(NeuralBaseClassifier):
 
     def __call__(self, batch):
         projection = self.string_projection(batch)
+        projection = projection.to(batch['inputs'].device)
         mask = (batch['inputs'] == self.pad).unsqueeze(2)
 
         x_value = self.fc_value(projection)
         x_value = self.dropout(x_value)
         x_value = x_value.masked_fill_(mask, 0)
+        x_value = self.batch_norm_value(x_value.transpose(1, 2)).transpose(1, 2)
         x_value = torch.relu(x_value)
 
         x_attn = self.fc_attn(projection)
         x_attn = self.dropout(x_attn)
         x_attn = x_attn.masked_fill_(mask, 0)
+        x_attn = self.batch_norm_attn(x_attn.transpose(1, 2)).transpose(1, 2)
         x_attn = torch.relu(x_attn)
 
         x_value = x_value.unsqueeze(1)
