8e938c51c5f6ce4499dc411abf66e1d65df2f246
Takuya Makino <takuyamakino15@gmail.com>
2021-02-22 18:06:12 +0900

    Add space tokenizer

---
+++ bunruija/tokenizers/space_tokenizer.py
@@ -0,0 +1,14 @@
+from bunruija.tokenizers import BaseTokenizer
+
+
+class SpaceTokenizer(BaseTokenizer):
+    def __init__(self, **kwargs):
+        super().__init__(name='space')
+
+    def __call__(self, text):
+        result = text.split(' ')
+        return result
+
+    def __repr__(self):
+        out = f'{self.__class__.__name__}()'
+        return out
