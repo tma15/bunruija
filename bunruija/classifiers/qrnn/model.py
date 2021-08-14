5597e26a0e2a6b86feb0f37314fe7be0f8d03fd6
Takuya Makino <takuyamakino15@gmail.com>
2021-08-14 15:54:22 +0900

    Add PRAD (#14)

--- bunruija/classifiers/qrnn/model.py
+++ bunruija/classifiers/qrnn/model.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 
 from bunruija.classifiers.classifier import NeuralBaseClassifier
@@ -24,11 +25,6 @@ class QRNNLayer(torch.nn.Module):
                 output_size * self.num_gates)
 
     def forward(self, x):
-#         x, batch_sizes, sorted_indices, unsorted_indices = x
-#         print('x', x.size())
-#         seq_len = batch_sizes.size(0)
-#         print('seq_len', seq_len)
-
         bsz = x.size(0)
         seq_len = x.size(1)
         window_tokens = [x]
@@ -45,18 +41,14 @@ class QRNNLayer(torch.nn.Module):
 
         z = torch.tanh(z)
         f = torch.sigmoid(f)
-#         print('z', z.size(), 'f', f.size())
-#         exit()
         seq_len = z.size(1)
 
         c = torch.zeros_like(z)
-#         print('C', c.size())
 
         if self.bidirectional:
             c = c.view(bsz, seq_len, 2, self.output_size)
             f = f.view(bsz, seq_len, 2, self.output_size)
             z = z.view(bsz, seq_len, 2, self.output_size)
-#             print('C', c.size())
             for t in range(seq_len):
                 if t == 0:
                     c[:, t, 0] = (1 - f[:, t, 0]) * z[:, t, 0]
@@ -68,9 +60,6 @@ class QRNNLayer(torch.nn.Module):
                 else:
                     c[:, t, 0] = f[:, t, 0] * c[:, t + 1, 0].clone() + (1 - f[:, t, 0]) * z[:, t, 0]
             c = c.view(bsz, seq_len, 2 * self.output_size)
-#             print('C', c.size())
-#             exit()
-#             pass
         else:
             for t in range(seq_len):
                 if t == 0:
@@ -79,8 +68,6 @@ class QRNNLayer(torch.nn.Module):
                     c[:, t] = f[:, t] * c[:, t -1].clone() + (1 - f[:, t]) * z[:, t]
 
         h = torch.sigmoid(o) * c
-#         output = PackedSequence(h, batch_sizes, sorted_indices, unsorted_indices)
-#         return output
         return h
 
 
@@ -92,7 +79,6 @@ class QRNN(NeuralBaseClassifier):
         self.dim_hid = kwargs.get('dim_hid', 128)
         self.window_size = kwargs.get('window_size', 3)
 
-        self.embed = torch.nn.Embedding(10000, self.dim_emb, padding_idx=0)
         self.layers = torch.nn.ModuleList()
         self.bidirectional = kwargs.get('bidirectional', True)
         num_layers = kwargs.get('num_layers', 2)
@@ -108,12 +94,18 @@ class QRNN(NeuralBaseClassifier):
                 window_size=self.window_size,
                 bidirectional=self.bidirectional))
 
-#         self.pooler = torch.nn.MaxPool2d(
-#             (2 * self.dim_hid if self.bidirectional else self.dim_hid),
-#         )
-
     def init_layer(self, data):
         self.pad = 0
+        max_input_idx = 0
+        for data_i in data:
+            max_input_idx = max(max_input_idx, np.max(data_i['inputs']))
+
+        self.embed = torch.nn.Embedding(
+            max_input_idx + 1,
+            self.dim_emb,
+            padding_idx=0,
+        )
+
         self.out = torch.nn.Linear(
             2 * self.dim_hid if self.bidirectional else self.dim_hid,
             len(self.labels),
@@ -123,28 +115,12 @@ class QRNN(NeuralBaseClassifier):
         src_tokens = batch['inputs']
         lengths = (src_tokens != self.pad).sum(dim=1)
 
-#         print(src_tokens)
         x = self.embed(src_tokens)
-#         print('embed', x.size())
-#         packed = torch.nn.utils.rnn.pack_padded_sequence(
-#             x,
-#             lengths,
-#             batch_first=True,
-#             enforce_sorted=False
-#         )
-#         self.layers[0](packed)
-#         exit()
         for layer in self.layers:
             x = layer(x)
-#         print('x', x.size())
-#         x = x[:, 0]
-#         x = self.pooler(x)
         x = torch.nn.functional.adaptive_max_pool2d(
             x,
             (1, 2 * self.dim_hid if self.bidirectional else self.dim_hid))
         x = x.squeeze(1)
-#         print('x', x.size())
         x = self.out(x)
-#         print('x', x.size())
-#         exit()
         return x
