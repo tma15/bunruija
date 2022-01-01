69553365e51c3b0469757517d57ed3028d4f6ea7
Takuya Makino <takuyamakino15@gmail.com>
2022-01-01 13:28:24 +0900

    Move to poetry-based installation

--- build.py
+++ build.py
@@ -1,19 +1,13 @@
 import os
-from setuptools import Extension, setup, find_packages
+from setuptools import Extension
+from distutils.command.build_ext import build_ext
 
 from Cython.Build import cythonize
 import numpy
 
+def build(setup_kwargs):
 
-# CXX_SOURCES = [os.path.join('csrc', x) for x in os.listdir('csrc') if x.endswith('cc')]
-# print(CXX_SOURCES)
-
-
-setup(
-    name='bunruija',
-    version='0.0.0',
-    packages=find_packages(),
-    ext_modules=cythonize([
+    ext_modules = cythonize([
         Extension(
             'bunruija.modules.vector_processor',
             sources=(
@@ -35,23 +29,10 @@ setup(
             extra_compile_args=['-std=c++11'],
             extra_link_args=['-std=c++11'],
         )
-    ]),
-    install_requires=[
-        'lightgbm',
-        'mecab-python3==0.996.5',
-        'mmh3',
-        'pyyaml',
-        'torch>=1.6.0',
-        'transformers>=3.5.1',
-        'scikit-learn>=0.23.2',
-        'unidic-lite',
-    ],
-    entry_points={
-        'console_scripts': [
-            'bunruija-evaluate = bunruija_cli.evaluate:cli_main',
-            'bunruija-predict = bunruija_cli.predict:cli_main',
-            'bunruija-preprocess = bunruija_cli.preprocess:cli_main',
-            'bunruija-train = bunruija_cli.train:cli_main',
-        ],
-    },
-)
+    ])
+
+    setup_kwargs.update({
+        'ext_modules': ext_modules,
+        'cmdclass': {'build_ext': build_ext}
+    })
+
