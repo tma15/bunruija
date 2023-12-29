df8d718263d1a89b38f6ddbaec3d20af3f6a389e
Takuya Makino <takuyamakino15@gmail.com>
2023-12-29 09:21:57 +0900

    Comment out unused modules

--- build.py
+++ build.py
@@ -2,11 +2,11 @@ from setuptools import Extension  # type: ignore
 from distutils.command.build_ext import build_ext
 
 from Cython.Build import cythonize  # type: ignore
-import numpy
+
+# import numpy
 
 
 def build(setup_kwargs):
-
     ext_modules = cythonize(
         [
             Extension(
@@ -20,16 +20,16 @@ def build(setup_kwargs):
                 extra_compile_args=["-std=c++11"],
                 extra_link_args=["-std=c++11"],
             ),
-            Extension(
-                "bunruija.classifiers.prado.string_projector",
-                sources=(
-                    ["bunruija/classifiers/prado/string_projector.pyx"]
-                    + ["csrc/string_projector_op.cc", "csrc/string_util.cc"]
-                ),
-                include_dirs=["include", numpy.get_include()],
-                extra_compile_args=["-std=c++11"],
-                extra_link_args=["-std=c++11"],
-            ),
+            # Extension(
+            #     "bunruija.classifiers.prado.string_projector",
+            #     sources=(
+            #         ["bunruija/classifiers/prado/string_projector.pyx"]
+            #         + ["csrc/string_projector_op.cc", "csrc/string_util.cc"]
+            #     ),
+            #     include_dirs=["include", numpy.get_include()],
+            #     extra_compile_args=["-std=c++11"],
+            #     extra_link_args=["-std=c++11"],
+            # ),
         ]
     )
 
