98dbdd2040a8351bb8536d26875bfe2e2b534f65
Takuya Makino <takuyamakino15@gmail.com>
2023-12-29 16:40:38 +0900

    Use datasets.load_dataset for data preparation

--- example/livedoor_corpus/create_data.py
+++ example/livedoor_corpus/create_data.py
@@ -1,68 +1,32 @@
+from argparse import ArgumentParser
 import csv
-import sys
-import glob
+from pathlib import Path
 
-from sklearn.model_selection import train_test_split
-
-
-def loader(data_dir):
-    files = glob.glob(f"{data_dir}/*/*.txt")
-    categories = []
-    titles = []
-    for fname in files:
-        elems = fname.split("/")
-        category = elems[-2]
-        with open(fname) as f:
-            next(f)
-            next(f)
-            title = next(f).rstrip()
-        categories.append(category)
-        titles.append(title)
-    print("#Files:", len(titles))
-    return categories, titles
+from datasets import load_dataset
+from loguru import logger  # type: ignore
 
 
-def train_dev_test_split(categories, titles):
-    titles_train, titles_test, categories_train, categories_test = train_test_split(
-        titles, categories, test_size=0.33, random_state=42
-    )
-
-    titles_train, titles_dev, categories_train, categories_dev = train_test_split(
-        titles_train, categories_train, test_size=0.33, random_state=42
-    )
-
-    return (
-        categories_train,
-        titles_train,
-        categories_dev,
-        titles_dev,
-        categories_test,
-        titles_test,
-    )
-
-
-def write_csv(categories, titles, name):
+def write_csv(samples: list[dict], name: Path):
     with open(name, "w") as f:
         writer = csv.writer(f)
-        for category, title in zip(categories, titles):
-            writer.writerow([category, title])
+        for sample in samples:
+            writer.writerow([f"{sample['category']}", sample["title"]])
+        logger.info(f"{name}")
 
 
 def main():
-    categories, titles = loader(sys.argv[1])
+    parser = ArgumentParser()
+    parser.add_argument("--output_dir", default="example/livedoor_corpus", type=Path)
+    args = parser.parse_args()
 
-    (
-        categories_train,
-        titles_train,
-        categories_dev,
-        titles_dev,
-        categories_test,
-        titles_test,
-    ) = train_dev_test_split(categories, titles)
-
-    write_csv(categories_train, titles_train, "train.csv")
-    write_csv(categories_dev, titles_dev, "dev.csv")
-    write_csv(categories_test, titles_test, "test.csv")
+    dataset = load_dataset(
+        "shunk031/livedoor-news-corpus",
+        random_state=0,
+        shuffle=True,
+    )
+    write_csv(dataset["train"], args.output_dir / "train.csv")
+    write_csv(dataset["validation"], args.output_dir / "dev.csv")
+    write_csv(dataset["test"], args.output_dir / "test.csv")
 
 
 if __name__ == "__main__":
